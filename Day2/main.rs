74ac83aaa54e27486f92ed92f3b58fb638e46550
Chase Killorin <chase.k@metactf.com>
2023-12-11 17:47:26 -0500

    Day 2 pt 2 done

--- Day2/main.rs
+++ Day2/main.rs
@@ -1,22 +1,18 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-// use std::collections::HashMap;
 
 fn main() {
-
     println!("Day 2 hour 0\nNo idea what I'm doing");
 
+    // Define maxes for nums
     const RED_MAX :i32 = 12;
     const GREEN_MAX :i32 = 13;
     const BLUE_MAX :i32 = 14;
 
+    // Total for pt1 power_total for pt2
     let mut total = 0;
-    // let mut scores: HashMap<String, i32> = HashMap::new();
-
-    // scores.insert(String::from("red"), 0);
-    // scores.insert(String::from("green"), 0);
-    // scores.insert(String::from("blue"), 0);
+    let mut power_total = 0; 
 
     // File hosts.txt must exist in the current path
     let Ok(lines) = read_lines("./input.txt") else  {
@@ -24,23 +20,29 @@ fn main() {
             return;
         };
 
-    // Consumes the iterator, returns an (Optional) String
 
+    // For ever line in the file
     for line in lines {
         let Ok(game) = line else{continue;};
 
+        // Keep track of highest values we see
         let mut red_highest = 0;
         let mut green_highest = 0;
         let mut blue_highest = 0;
         
+        // Split line by : and then by ;
         let no_game = &game[game.find(':').unwrap()+2..];
         let semicolon_split_values = no_game.split(';');
 
+        // For every set/handful of cubes
         for run in semicolon_split_values{
 
+            // For every pair you see
             let comma_split_values = run.split(',');
             for mut pair in comma_split_values{
 
+                // Trim and match to pair
+                // Overwrite highest if needed
                 pair = pair.trim();
                 if pair.contains("red"){
                     
@@ -57,31 +59,26 @@ fn main() {
                 } else if pair.contains("blue"){
     
                     // println!("Pair: {}", pair);
-                    // println!("Pair: {}", pair.split(' ').nth(0).unwrap());
                     if blue_highest < pair.split(' ').nth(0).unwrap().parse::<i32>().unwrap(){
                         blue_highest = pair.split(' ').nth(0).unwrap().parse::<i32>().unwrap();
                     }
                 }
             }
-
-            // println!("Red: {}", red_total);
-            // println!("Green: {}", green_total);
-            // println!("Blue: {}", blue_total);
-
         }
 
+        // If highest values are less than maxes then add to total
         if red_highest <= RED_MAX && green_highest <= GREEN_MAX && blue_highest <= BLUE_MAX{
             total += game.split(':').nth(0).unwrap().split(' ').last().unwrap().parse::<i32>().unwrap();
-            println!("Total added: {}, Game: {}", total, game.split(':').nth(0).unwrap().split(' ').last().unwrap().parse::<i32>().unwrap());
+            // println!("Total added: {}, Game: {}", total, game.split(':').nth(0).unwrap().split(' ').last().unwrap().parse::<i32>().unwrap());
         }
 
-        // println!("Total: {}", total);
+        // Add to power total
+        power_total += red_highest * green_highest * blue_highest;
     }
 
-
     println!("Total: {}", total);
-    // println!("Red {}", scores.get("red").unwrap());
-
+    println!("Power Total: {}", power_total);
+    
 }
 
 // The output is wrapped in a Result to allow matching on errors
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-My advent of code solutions in Rust (I have no rust proficiency) 
+My advent of code solutions in Rust (I have no rust skill) 
 I am mid finals so that is why I am behind right now
 
 | Day  |Sol1|Sol2|
 |-----|----|----|
 |1|Y|N|
-|2|Y|Working on|
+|2|Y|Y|
+|3|Working|N|
